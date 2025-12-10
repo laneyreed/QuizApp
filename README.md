fc7758b024a8f0c623fdec4b3c86c18e3ce4aebe
Shanreed25 <tralainereed@gmail.com>
2025-12-09 20:10:39 -0800

    added data, user stories and datamodel

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Quiz App
+
+
+
+
+
+<!-- wedidit.org -->
---
+++ main.py
@@ -0,0 +1,17 @@
+questions = [
+    {
+        "question": "What is the capital of France?",
+        "choices": {"A": "London", "B": "Berlin", "C": "Paris", "D": "Madrid"},
+        "answer": {"C": "Paris"}
+    },
+    {
+        "question": "Who painted the Mona Lisa?",
+        "choices":{"A": "Vincent van Gogh", "B": "Leonardo da Vinci", "C": "Pablo Picasso", "D": "Claude Monet"},
+        "answer": {"B": "Leonardo da Vinci"}
+    },
+    {
+        "question": "What is the largest planet in our solar system?",
+        "choices": {"A": "Earth", "B": "Mars", "C": "Jupiter", "D": "Saturn"},
+        "answer": {"C": "Jupiter"}
+    }
+]
